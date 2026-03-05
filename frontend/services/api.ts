9125c24d5ec40d0e1698690541840d63be086c11
Chirath <hakmanagedinuka@gmail.com>
2026-03-06 02:19:38 +0530

    fix: restore API URL to local network IP

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -5,7 +5,7 @@ import { SaveBillPayload } from '@/types/bill.types';
 
 // IMPORTANT: Change this to YOUR computer's IP address
 // Find IP: Windows (ipconfig) | Mac (ifconfig) | Linux (hostname -I)
-const API_BASE_URL = "http://localhost:8000/api";
+const API_BASE_URL = "http://192.168.1.2:8000/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
