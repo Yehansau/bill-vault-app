6c15fd7a18a7102e806af05f81a6b45597e05a26
Yuwani <yuwaniranaweera@gmail.com>
2026-03-07 00:36:52 +0530

    Updated welcome screen UI and added a photo

--- frontend/app/auth/auth-choice.tsx
+++ frontend/app/auth/auth-choice.tsx
@@ -3,7 +3,7 @@ import { LinearGradient } from "expo-linear-gradient";
 import { router } from "expo-router";
 import { useState } from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
-import logo from "../../assets/images/LogoPicture.png";
+import girlImage from "../../assets/images/woman_bills.png"; 
 
 const AuthChoiceScreen = () => {
   const [loading, setLoading] = useState(false);
@@ -22,36 +22,42 @@ const AuthChoiceScreen = () => {
 
   return (
     <LinearGradient
-      colors={["#944ABC", "#3B0856"]}
+      colors={["#9B4FD6", "#3B0856"]}
       start={{ x: 0, y: 0 }}
-      end={{ x: 1, y: 0 }}
+      end={{ x: 0, y: 1 }}
       style={styles.container}
     >
-      <View style={styles.content}>
-        <Image source={logo} style={styles.logo} />
-        <Text style={styles.title}>BILLVAULT</Text>
-        <Text style={styles.subtitle}>NEVER LOSE A BILL AGAIN</Text>
+      {/* Top Text */}
+      <View style={styles.header}>
+        <Text style={styles.welcome}>Welcome to</Text>
+        <Text style={styles.brand}>BillVault.</Text>
       </View>
 
-      <View style={styles.buttonContainer}>
+      {/* Image */}
+      <View style={styles.imageContainer}>
+        <Image source={girlImage} style={styles.image} />
+      </View>
 
+      {/* Buttons */}
+      <View style={styles.buttonContainer}>
         <CustomButton
           title="Login"
           onPress={handleLogin}
           loading={loading}
           variant="secondary"
+          style={{ width: "80%" }}
         />
-      </View>
 
-      <View style={styles.createAccountbuttonContainer}>
+        <View style={styles.spacing} />
+
         <CustomButton
           title="Create Account"
           onPress={handleCreateAccount}
           loading={loading}
           variant="secondary"
+          style={{ width: "80%" }}
         />
-      </View>  
-
+      </View>
     </LinearGradient>
   );
 };
@@ -59,45 +65,49 @@ const AuthChoiceScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    justifyContent: "center",
+    paddingHorizontal: 25,
+    paddingTop: 80,
+    justifyContent: "flex-start",
   },
-  content: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+
+  header: {
+    marginTop: 20,
+    marginLeft: 35
   },
-  logo: {
-    height: 130,
-    width: 130,
-    marginBottom: 30,
+
+  welcome: {
+    fontSize: 26,
+    fontWeight: "700",
+    color: "black",
   },
-  title: {
-    fontSize: 36,
+
+  brand: {
+    fontSize: 32,
     fontWeight: "bold",
     color: "white",
-    marginBottom: 10,
+    marginTop: 5,
   },
-  subtitle: {
-    fontSize: 14,
-    color: "white",
-    textAlign: "center",
-    marginBottom: 10,
+
+  imageContainer: {
+    alignItems: "center",
+    marginTop: 60
   },
+
+  image: {
+    width: 260,
+    height: 260,
+    resizeMode: "contain",
+  },
+
   buttonContainer: {
-    paddingHorizontal: 30,
-    paddingBottom: 30,
-    marginTop: -30,
-    marginBottom: 20,
-    width: "92%",
-    alignSelf: "center"
+    paddingBottom: 50,
+    marginTop: 80,
+    alignItems: "center",
   },
-  
-  createAccountbuttonContainer: {
-    marginBottom: 150,
-    width: "75%",
-    alignSelf: "center"
-  }
+
+  spacing: {
+    height: 18,
+  },
 });
 
 export default AuthChoiceScreen;
