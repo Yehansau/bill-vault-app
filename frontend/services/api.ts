2898ee5c1f882473143e9ce3a3f9a4b94434378e
Yuwani <yuwaniranaweera@gmail.com>
2026-03-21 14:49:59 +0530

    Created the reset-password page

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -6,7 +6,7 @@ import { SaveBillPayload } from "@/types/bill.types";
 // IMPORTANT: Change this to YOUR computer's IP address
 // Find IP: Windows (ipconfig) | Mac (ifconfig) | Linux (hostname -I)
 
-const API_BASE_URL = "http://10.79.228.206:8000/api";
+const API_BASE_URL = "http://10.65.96.50:8000/api";
 
 
 const api = axios.create({
